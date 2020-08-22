17f2981da22db40b7ee7c61878eadd08be5e044a
shayanypn <shayanypn@gmail.com>
2020-08-22 20:21:43 +0430

    Updates

---
+++ precache-manifest.166e3000f684b7753cb70555b1f4214b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800137e2b305ff3fb4b7da7578aa15bf",
+    "url": "/bookipy-reviews/index.html"
+  },
+  {
+    "revision": "0c441c4d6cb63fc48737",
+    "url": "/bookipy-reviews/static/css/main.c456f928.chunk.css"
+  },
+  {
+    "revision": "5537dd9094336ce906b4",
+    "url": "/bookipy-reviews/static/js/2.4d200156.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookipy-reviews/static/js/2.4d200156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c441c4d6cb63fc48737",
+    "url": "/bookipy-reviews/static/js/main.e1edf8cd.chunk.js"
+  },
+  {
+    "revision": "7ab6f2cfc1797bfeb16f",
+    "url": "/bookipy-reviews/static/js/runtime-main.de9df2cd.js"
+  },
+  {
+    "revision": "c74888e8d90cf54838456dd28bde5727",
+    "url": "/bookipy-reviews/static/media/AIRBNB.c74888e8.svg"
+  },
+  {
+    "revision": "7802808d7383dd1bb0b214c5058d593c",
+    "url": "/bookipy-reviews/static/media/thumb-down.7802808d.svg"
+  },
+  {
+    "revision": "962766a5cf47244dd1dd63cc947e9525",
+    "url": "/bookipy-reviews/static/media/thumb-up.962766a5.svg"
+  }
+]);
