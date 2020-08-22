fd3ecb1faf1f177828443f65c378f7262d7e64ff
shayanypn <shayanypn@gmail.com>
2020-08-22 07:46:36 +0430

    add structures and basics

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div class="container">
+      <main>
+        <h2>ID: 091021</h2>  
+        <h1>La Casa de las Flores</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
