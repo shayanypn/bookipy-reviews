8659db93cde3c29b0795122cf5981a91fdc40447
shayanypn <shayanypn@gmail.com>
2020-08-22 21:15:28 +0430

    specify a url for pagination

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -33,13 +33,13 @@ const App = () => {
           <nav aria-label="Page navigation example">
             <ul className="pagination justify-content-end">
               <li className="page-item disabled">
-                <a className="page-link" href="#" tabIndex="-1" aria-disabled="true">Previous</a>
+                <a className="page-link" href="/" tabIndex="-1" aria-disabled="true">Previous</a>
               </li>
-              <li className="page-item"><a className="page-link" href="#">1</a></li>
-              <li className="page-item"><a className="page-link" href="#">2</a></li>
-              <li className="page-item"><a className="page-link" href="#">3</a></li>
+              <li className="page-item"><a className="page-link" href="/">1</a></li>
+              <li className="page-item"><a className="page-link" href="/">2</a></li>
+              <li className="page-item"><a className="page-link" href="/">3</a></li>
               <li className="page-item">
-                <a className="page-link" href="#">Next</a>
+                <a className="page-link" href="/">Next</a>
               </li>
             </ul>
           </nav>
