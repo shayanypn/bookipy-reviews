0145ff397078a622928ac8ccb22323c97ab510b4
shayanypn <shayanypn@gmail.com>
2020-08-22 21:26:14 +0430

    re structure project

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,3 +1,30 @@
-import Review from './Review';
+import React from 'react';
+import thumbDown from '../../icons/thumb-down.svg';
+import thumbUp from '../../icons/thumb-up.svg';
+import AIRBNB from '../../icons/AIRBNB.svg';
+import './main.css';
+
+const Review = () => {
+  return (
+    <article>
+    	<header>
+			<span className="badge"><b>4.3</b>/5</span>
+			<img src={AIRBNB} alt="website name" />
+			<h5>Very nice host, and quite chill place.!</h5>
+    	</header>
+    	<summary>
+			<p>Morbi porttitor nisl ipsum, a facilisis purus euismod eu. Praesent consequat interdum nisi ut auctor. Sed posuere est porta neque pretium viverra. Nulla vel finibus nulla.</p>
+			<ul>
+				<li><img src={thumbUp} alt="agree point" /> Nulla dictum ligula ac tortor egestas, sit amet ullamcorper risus sollicitudin. Morbi vitae leo accumsan, interdum ligula non, placerat ligula. Ut faucibus congue purus, vitae semper</li>
+				<li><img src={thumbDown} alt="disagree point" /> Nulla dictum ligula ac tortor egestas, sit amet ullamcorper risus sollicitudin. Morbi vitae leo accumsan, interdum ligula non, placerat ligula. Ut faucibus congue purus, vitae semper</li>
+			</ul>
+    	</summary>
+		<footer>
+			<strong>Alissa Stacey</strong>
+			<time>Reviewed 13 December 2019</time>
+		</footer>
+    </article>
+  );
+}
 
 export default Review;
