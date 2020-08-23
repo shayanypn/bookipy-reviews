8967eecbc23bd0b174289ab33ef6e4de2a0a8acc
shayanypn <shayanypn@gmail.com>
2020-08-23 11:20:06 +0430

    implement review component and apply test

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,30 +1,48 @@
 import React from 'react';
+import Moment from 'react-moment';
 import thumbDown from '../../icons/thumb-down.svg';
 import thumbUp from '../../icons/thumb-up.svg';
 import AIRBNB from '../../icons/AIRBNB.svg';
+import BOOKINGCOM from '../../icons/BOOKINGCOM.svg';
+import HOLIDU from '../../icons/HOLIDU.svg';
 import './main.css';
 
-const Review = () => {
-  return (
-    <article>
-    	<header>
-			<span className="badge"><b>4.3</b>/5</span>
-			<img src={AIRBNB} alt="website name" />
-			<h5>Very nice host, and quite chill place.!</h5>
-    	</header>
-    	<summary>
-			<p>Morbi porttitor nisl ipsum, a facilisis purus euismod eu. Praesent consequat interdum nisi ut auctor. Sed posuere est porta neque pretium viverra. Nulla vel finibus nulla.</p>
-			<ul>
-				<li><img src={thumbUp} alt="agree point" /> Nulla dictum ligula ac tortor egestas, sit amet ullamcorper risus sollicitudin. Morbi vitae leo accumsan, interdum ligula non, placerat ligula. Ut faucibus congue purus, vitae semper</li>
-				<li><img src={thumbDown} alt="disagree point" /> Nulla dictum ligula ac tortor egestas, sit amet ullamcorper risus sollicitudin. Morbi vitae leo accumsan, interdum ligula non, placerat ligula. Ut faucibus congue purus, vitae semper</li>
-			</ul>
-    	</summary>
-		<footer>
-			<strong>Alissa Stacey</strong>
-			<time>Reviewed 13 December 2019</time>
-		</footer>
-    </article>
-  );
-}
+const iconMap = {
+	'AIRBNB': AIRBNB,
+	'BOOKINGCOM': BOOKINGCOM,
+	'HOLIDU': HOLIDU
+};
+
+const Review = ({
+	headline,
+	score,
+	channel,
+	comment,
+	positiveFeedback,
+	negativeFeedback,
+	author,
+	publishedAt
+}) => (
+  <article>
+    <header>
+      <span className="badge"><b>{score}</b>/5</span>
+      <img src={iconMap[channel]} alt="website name" />
+      <h5>{headline}</h5>
+    </header>
+    <summary>
+      <p>{comment}</p>
+      <ul>
+      	{positiveFeedback && <li><img src={thumbUp} alt="agree point" /> {positiveFeedback}</li> }
+      	{negativeFeedback && <li><img src={thumbDown} alt="disagree point" /> {negativeFeedback}</li> }
+      </ul>
+    </summary>
+    <footer>
+      <strong>{author}</strong>
+      <time>
+      	Reviewed <Moment format="DD MMM YYYY" element="span">{publishedAt}</Moment>
+      </time>
+    </footer>
+  </article>
+)
 
 export default Review;
